627cffd2aec3b2b19a512e64ff34898a248417e8
Fatma Orsun <fatmaorsun2@gmail.com>
2020-12-26 12:31:10 -0500

    "Prices and Footer"

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           {/*This is Default Page */}
           <Route path="/">
             <Home />
-            <About />
+      
           </Route>
         </Switch>
 
--- src/product/Product.js
+++ src/product/Product.js
@@ -1,14 +1,33 @@
 import React from "react";
+import { useStateValue } from "../StateProvider";
+import "./Product.css";
 
-function Product(){
-
-    return (
-        <div >
-
-        </div>
-
-    );
-
+function Product({ id, benefit, type, price, image }) {
+  const [{ cart }, dispatch] = useStateValue();
+  const addToCart = () => {
+    dispatch({
+      type: "ADD_TO_CART",
+      item: {
+        type: type,
+        benefit: benefit,
+        price: price,
+      },
+    });
+  };
+  return (
+    <div className="product">
+      <div className="div product__info">
+        <h1>{type}</h1>
+        <p>{benefit}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+      </div>
+      <img className="card_img" src={image} alt="" width="25%" />
+      <button onClick={addToCart}>Add to Cart</button>
+    </div>
+  );
 }
 
 export default Product;
